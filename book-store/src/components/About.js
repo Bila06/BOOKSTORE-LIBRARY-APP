d77722c7f2de9d7bf9c27ed563a5ce19d2f721cb
Bila06 <davidbila7ks@gmail.com>
2023-11-08 12:21:03 +0200

    about us page

--- book-store/src/components/About.js
+++ book-store/src/components/About.js
@@ -1,11 +1,24 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import picture from "./drop.jpg";
+import brog from "./brog.webp";
+
 
 const About = () => {
-  return <div>
+  return <div style={{backgroundImage: `url(${brog})`,
+  height: "665px",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  
+  }}>
       <Box display="flex" flexDirection="column" alignItems="center">
-        <Typography sx={{fontFamily:"fantasy"}} variant="h2"> this is crud application for book store</Typography>
-        <Typography sx={{fontFamily:"fantasy"}} variant="h3"> created using mern stack</Typography>
+        <Typography sx={{marginTop:3}} variant="h1"> BOOKSTORE LIBRARY APP </Typography>
+        <Typography sx={{marginTop:15}} variant="h3"> GET YOUR BOOK TODAY !!</Typography>
+        <Typography sx={{marginTop:10}} variant="h5"> We provide you with the knowledge 
+        you need.</Typography>
+        <Typography  variant="h5" >Struggling with school work! come get the book that will help  you.</Typography>
+        <Typography  variant="h5">You struggling to get more information to teach! come get a book that will help you.</Typography>
+        <Typography variant="h5"> You want good a good story book! we got you </Typography>
 
       </Box>
     </div>;
